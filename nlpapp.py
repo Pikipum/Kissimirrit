4643e4aac5f20578bc64b0e25b1f6e86188392dd
Salla Korhonen <salla.x.korhonen@helsinki.fi>
2021-01-27 12:21:01 +0200

    extracting html

--- nlpapp.py
+++ nlpapp.py
@@ -1,1 +1,17 @@
-print("Hello World!!")
+import nltk, re, pprint
+from nltk import word_tokenize
+from urllib import request
+from bs4 import BeautifulSoup
+
+def main():
+
+        url = "https://www.hs.fi/kotimaa/art-2000007759719.html"
+        html = request.urlopen(url).read().decode('utf8')
+        raw = BeautifulSoup(html, 'html.parser').get_text()
+        tokens = word_tokenize(raw)
+        soup = BeautifulSoup(html, 'html.parser')
+        print(soup.prettify())
+
+
+
+main()
