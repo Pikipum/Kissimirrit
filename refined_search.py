40375c47dfe92c1e778b20d61618e8e0a644ddaf
Pikipum <markus.tervonen@hotmail.com>
2021-02-10 16:15:31 +0200

    Added functionality to search stemmed or unstemmed documents

--- refined_search.py
+++ refined_search.py
@@ -33,9 +33,12 @@ def main():
 
     articles.pop()
 
-    global articlenames, gv, g_matrix
+    global articlenames, gv, gv_stemmed, g_matrix, g_matrix_stemmed
     articlenames = list(corpus_with_names.keys())
     articledata = list(corpus_with_names[name] for name in articlenames)
+
+
+
 
     documents = stem_documents()
     article_names = list(documents.keys())
@@ -45,14 +48,22 @@ def main():
     gv = TfidfVectorizer(lowercase=True, sublinear_tf=True, use_idf=True, norm="l2")
     g_matrix = gv.fit_transform(articledata).T.tocsr()
 
+    gv_stemmed = TfidfVectorizer(lowercase=True, sublinear_tf=True, use_idf=True, norm="l2")
+    g_matrix_stemmed = gv_stemmed.fit_transform(stemmed_data).T.tocsr() # Create a separate matrix for the stemmed data
+
+
     cv = CountVectorizer(lowercase=True, binary=True)
     gv._validate_vocabulary()
+    gv_stemmed._validate_vocabulary() # Validate stemmed vocabulary
     sparse_matrix = cv.fit_transform(articles)
     binary_dense_matrix = cv.fit_transform(articles).T.todense()
     dense_matrix = cv.fit_transform(articles).T.todense()
 
     global terms
     terms = gv.get_feature_names()
+
+    global stemmed_terms
+    stemmed_terms = gv_stemmed.get_feature_names() # Get the stemmed feature names
 
     global sparse_td_matrix
     sparse_td_matrix = sparse_matrix.T.tocsr()
@@ -65,39 +76,59 @@ def main():
 
     global t2i
     t2i = cv.vocabulary_
-      
+
+#    query_stemmed = input("Search stemmed documents? y/n: ")  # Asks whether user would like to search stemmed results
+#    if query_stemmed == "y":
+#        stemmed = True
+#    else:
+#        stemmed = False
+
+
 
     while True:
+        stemmed = True
         boolean = 0
         inp = input("Search for a document: ")  # asks user for input
         if inp == '':
             break
         if re.match('["][\w\s]+["]', inp): # Checks if input has quotation marks
             inp = re.sub('"', '', inp) # Removes quotation marks
+            stemmed = False # Sets the input to search unstemmed documents (exact matches)
 
-        if check_for_unknown_words(inp) == True:
+        if stemmed == True: # Stem the query
+            stemmer = SnowballStemmer("english")
+            inp = stemmer.stem(inp)
+
+        if check_for_unknown_words(inp, stemmed) == True:
             for t in inp.split():
                 if t in d.keys():
                     retrieve_articles(inp)
                     boolean += 1
                     break
             if boolean == 0 and len(inp.split()) == 1:
-                search_wikicorpus(inp)
+                search_wikicorpus(inp, stemmed)
             elif boolean == 0:
                 term = inp.split()
                 gv.ngram_range = (len(term), len(term))
                 g_matrix = gv.fit_transform(articledata).T.tocsr()
-                search_wikicorpus(inp)
+                search_wikicorpus(inp, stemmed)
                 
 
 
-def check_for_unknown_words(query):
+def check_for_unknown_words(query, stemmed):
     tokens = query.split()
-    for t in tokens:
-        if t not in terms and t not in d.keys():
-            print('Word "{}" is not found in corpus'.format(t))
-            #return ([0 0 0 0]) <-- add len(articles)
-            return False
+    if stemmed: # If stemmed is true, searches the stemmed terms. Otherwise continue to the unstemmed documents.
+        for t in tokens:
+            if t not in stemmed_terms and t not in d.keys():
+                print('Word "{}" is not found in corpus'.format(t))
+                # return ([0 0 0 0]) <-- add len(articles)
+                return False
+    else:
+        for t in tokens:
+            if t not in terms and t not in d.keys():
+                print('Word "{}" is not found in corpus'.format(t))
+                #return ([0 0 0 0]) <-- add len(articles)
+                return False
     return True
 
 
@@ -122,17 +153,23 @@ def retrieve_articles(inp):
             lines = articles[doc_idx].split("\n")
             print("Matching doc #{:d}: {:s}\n {:s}\n".format(i, lines[1], lines[2]))
 
-def search_wikicorpus(query_string):
-    # Vectorize query string
-    query_vec = gv.transform([query_string]).tocsc()
+def search_wikicorpus(query_string, stemmed):
 
-    # Cosine similarity
-    hits = np.dot(query_vec, g_matrix)
 
-    # Rank hits
-    ranked_scores_and_doc_ids = \
-        sorted(zip(np.array(hits[hits.nonzero()])[0], hits.nonzero()[1]),
-               reverse=True)
+    if stemmed == True: #
+        query_vec = gv_stemmed.transform([query_string]).tocsc() # Vectorize query string
+        hits_stemmed = np.dot(query_vec, g_matrix_stemmed) # Cosine similarity
+        ranked_scores_and_doc_ids = \
+            sorted(zip(np.array(hits_stemmed[hits_stemmed.nonzero()])[0], hits_stemmed.nonzero()[1]),
+                   reverse=True)
+
+    else:
+        query_vec = gv.transform([query_string]).tocsc() # Vectorize query string
+        hits = np.dot(query_vec, g_matrix) # Cosine similarity
+        ranked_scores_and_doc_ids = \
+            sorted(zip(np.array(hits[hits.nonzero()])[0], hits.nonzero()[1]),
+                   reverse=True)
+
 
     # Output result
     print("Your query '{:s}' matches the following documents:".format(query_string))
